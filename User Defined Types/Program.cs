53a91f00df8a256f2db401abf607d4c91b269a3b
Kulpot <kulpotwahu@gmail.com>
2023-08-14 20:22:09 +0800

    Add project files.

---
+++ User Defined Types/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=HY8YIU_xCCA&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=2
+//
+
+/*
+ * static void P(this object o)
+{
+    Console.WriteLine(o);
+}
+ * 
+ */
+
+class Cow
+{
+    int numSteaks;
+    int mooCount;
+    double pounds;
+    public void Moo() 
+    { 
+        "Moo".P(); 
+        mooCount++;
+    }
+
+}
+
+
+
+class MainClass
+{
+    static void Main()
+    {
+        Cow betsy = new Cow();
+        betsy.Moo();
+    }
+    
+}
+
